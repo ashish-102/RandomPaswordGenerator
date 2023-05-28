64c74c5bff4030dce26bc5f9dc548ab5e1b14145
Ashish <64186295+ashish-102@users.noreply.github.com>
2023-05-28 18:25:28 +0530

    initail change

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,1 +1,9 @@
+import random
+chars = '0qweQWE1rtyRTY2uiopUIOP3[]4asdASD5fghFGH6jklJKL7zxcZXC8vbnVBN9mM@#$%&-_!'
+password = ''
 
+n = int(input())
+for i in range(n):
+    password += random.choice(chars) 
+
+print(password)
